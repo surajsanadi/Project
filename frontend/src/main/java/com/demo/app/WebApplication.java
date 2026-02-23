02674b072e2b0d159af28a48ba1061f99c69af8d
surajsanadi <surajsanadi008@gmail.com>
2026-02-23 17:45:17 +0530

    Update WebApplication.java

--- frontend/src/main/java/com/demo/app/WebApplication.java
+++ frontend/src/main/java/com/demo/app/WebApplication.java
@@ -3,7 +3,8 @@ package com.demo.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+#import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class WebApplication extends SpringBootServletInitializer {
